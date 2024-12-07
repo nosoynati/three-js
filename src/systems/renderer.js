c845ff3425e79f40b98055feb601b4a828db79c3
Natasha <natasha.berger@innew.la>
2024-12-07 19:16:47 -0300

    add: animation

--- src/systems/renderer.js
+++ src/systems/renderer.js
@@ -4,6 +4,7 @@ function createRenderer() {
   const renderer = new WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio)
+  
   return renderer
 }
 export {createRenderer};
