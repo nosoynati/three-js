1e1ed73d54fdb7e65095e77aa9c41e0698d161fe
nataberger <natasha.berger@innew.la>
2024-12-12 09:30:55 -0300

    fix: renderer

--- src/systems/Resizer.js
+++ src/systems/Resizer.js
@@ -1,9 +1,11 @@
 const setSize = (camera, container, renderer) => {
+  let ratio = window.devicePixelRatio
   camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
-
-  renderer.setSize = container.clientWidth, container.clientHeight;
   renderer.setPixelRatio = window.devicePixelRatio
+  renderer.setSize = container.clientWidth, container.clientHeight;
+ 
+
 
 }
 class Resizer {
--- src/systems/renderer.js
+++ src/systems/renderer.js
@@ -1,10 +1,9 @@
 import { WebGLRenderer } from 'three'
 
-
 function createRenderer() {
   const renderer = new WebGLRenderer({antialias: true});
   renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.setPixelRatio(window.devicePixelRatio)
+  //renderer.setPixelRatio(window.devicePixelRatio)
   renderer.physicallyCorrectLights = true
   
   
